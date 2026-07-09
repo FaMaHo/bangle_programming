f61ace37428619d9103aaccf6833fda6e9005b2b
FaMaHo <fs.mhosseiniiii@gmail.com>
2026-07-09 19:39:53 +0300

    Update Flutter lockfile and generated plugin registrations
    Result of running flutter pub get on the Mac before this repo moved to
    Windows; picks up newer transitive dependency versions. No direct
    dependency changes.

--- pulsewatch_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pulsewatch_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import flutter_blue_plus_darwin
 import flutter_local_notifications
 import mobile_scanner
-import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
@@ -17,7 +16,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
